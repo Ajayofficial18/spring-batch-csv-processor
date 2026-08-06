fd4148ed0a1fdeb30731f8d5f11150d659566959
Ajay Gour <ajgour14@gmail.com>
2026-08-06 12:05:56 +0530

    Initializing project

---
+++ src/test/java/com/batch_csv_processor/SpringBatchCsvProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batch_csv_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchCsvProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
