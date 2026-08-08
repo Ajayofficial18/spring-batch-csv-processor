7aa50c47b48ac5909a594ec030c34fc3d1d1232d
Ajay Gour <ajgour14@gmail.com>
2026-08-09 02:01:34 +0530

    Added InvalidDiscountException Handler

---
+++ src/main/java/com/batch_csv_processor/exception/InvalidDiscountException.java
@@ -0,0 +1,7 @@
+package com.batch_csv_processor.exception;
+
+public class InvalidDiscountException extends InvalidProductException{
+    public InvalidDiscountException(String message) {
+        super(message);
+    }
+}
