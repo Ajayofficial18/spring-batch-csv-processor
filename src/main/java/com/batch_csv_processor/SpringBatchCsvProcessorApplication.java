1bc695a8ec2ca21ae4c6e03ee64825f2c98579b3
Ajay Gour <ajgour14@gmail.com>
2026-08-09 01:58:39 +0530

    Modified main method

--- src/main/java/com/batch_csv_processor/SpringBatchCsvProcessorApplication.java
+++ src/main/java/com/batch_csv_processor/SpringBatchCsvProcessorApplication.java
@@ -2,12 +2,15 @@ package com.batch_csv_processor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class SpringBatchCsvProcessorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBatchCsvProcessorApplication.class, args);
+		System.out.println("App started");
 	}
 
 }
